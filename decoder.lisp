72c1968b22b06148c8f02f187c8b897c023b5df6
Ralph Schleicher <rs@ralph-schleicher.de>
2026-04-24 06:44:08 +0000

    (%make-float): Declare inline.

--- decoder.lisp
+++ decoder.lisp
@@ -377,7 +377,7 @@ The order of the pairs is reversed."
 	    #+cmucl
 	    (values (code-char high) (code-char low))))))))
 
-(defun %make-float (n q d)
+(defsubst %make-float (n q d)
   "Poor man's floating-point number conversion of significand N and
 decimal exponent Q.  Third argument D is the number of significant
 digits."
